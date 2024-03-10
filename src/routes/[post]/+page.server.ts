289b87e0998718e1df85c25fc7c69b7d89a8fd9f
emanusantos <emanuel.lmds1@gmail.com>
2024-03-10 19:53:14 -0300

    chore: use static adapter instead of vercel

--- src/routes/[post]/+page.server.ts
+++ src/routes/[post]/+page.server.ts
@@ -1,5 +1,12 @@
+import { findAllPosts } from '$lib/server/find-all-posts.js';
 import { findOnePost } from '$lib/server/find-one-post.js';
 import { markdownToHtml } from '$lib/server/markdown-to-html.js';
+
+export function entries() {
+	const posts = findAllPosts();
+
+	return posts.map((post) => ({ post: post.slug }));
+}
 
 export function load({ params }) {
 	const post = findOnePost(params.post);
