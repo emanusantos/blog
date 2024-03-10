e3e2b7d8a3edd7f340592a946ad6fea21c417573
emanusantos <emanuel.lmds1@gmail.com>
2024-03-10 15:51:42 -0300

    feat: add transitions to top and side bars

--- src/lib/components/Topbar.svelte
+++ src/lib/components/Topbar.svelte
@@ -1,21 +1,26 @@
 <script>
 	import { isSidebarOpen } from '$lib/writables/sidebar';
+	import { linear } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
 
 	function handleOpenSidebar() {
 		isSidebarOpen.set(true);
 	}
 </script>
 
-<header
-	class="w-full border b bg-gray-100 hidden lg:flex py-4 px-8"
-	class:lg:flex={!$isSidebarOpen}
->
-	<button
-		on:click|stopPropagation={handleOpenSidebar}
-		class="hidden lg:flex flex-col gap-1 cursor-pointer items-end"
+{#key $isSidebarOpen}
+	<header
+		class="w-full border b bg-gray-100 hidden lg:flex py-4 px-8"
+		class:lg:flex={!$isSidebarOpen}
+		transition:slide={{ duration: 200, easing: linear, axis: 'y' }}
 	>
-		<div class="w-8 h-[0.4rem] rounded-full bg-gray-500" />
-		<div class="w-8 h-[0.4rem] rounded-full bg-gray-500" />
-		<div class="w-8 h-[0.4rem] rounded-full bg-gray-500" />
-	</button>
-</header>
+		<button
+			on:click|stopPropagation={handleOpenSidebar}
+			class="hidden lg:flex flex-col gap-1 cursor-pointer items-end"
+		>
+			<div class="w-8 h-[0.4rem] rounded-full bg-gray-500" />
+			<div class="w-8 h-[0.4rem] rounded-full bg-gray-500" />
+			<div class="w-8 h-[0.4rem] rounded-full bg-gray-500" />
+		</button>
+	</header>
+{/key}
